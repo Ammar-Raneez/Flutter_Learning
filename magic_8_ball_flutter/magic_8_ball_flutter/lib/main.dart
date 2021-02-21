552bc79a017f0377795b79584279c80097a60a7f
Ammar <ammarraneez@gmail.com>
2021-02-21 21:22:02 +0530

    completed challenge project of Magic 8 Ball

--- magic_8_ball_flutter/magic_8_ball_flutter/lib/main.dart
+++ magic_8_ball_flutter/magic_8_ball_flutter/lib/main.dart
@@ -1,7 +1,58 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: BallPage(),
       ),
     );
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        backgroundColor: Color(0xff0000aa),
+        title: Center(
+          child: Text("Ask Me Anything"),
+        ),
+      ),
+      body: Ball()
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballNumber = 1;
+
+  void changeBallImage() {
+    this.ballNumber = Random().nextInt(5) + 1;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: (){
+                setState(() {
+                  changeBallImage();
+                });
+              },
+              child: Image.asset("images/ball$ballNumber.png"),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
