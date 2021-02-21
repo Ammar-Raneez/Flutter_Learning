8717f61484abe61cfb71070c629da82e00c4246c
Ammar <ammarraneez@gmail.com>
2021-02-21 23:19:14 +0530

    7 buttons different sound

--- xylophone_flutter/lib/main.dart
+++ xylophone_flutter/lib/main.dart
@@ -1,15 +1,73 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
+import 'package:audioplayers/audio_cache.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  void playSound(String note) {
+    final player = AudioCache();
+    player.play(note);
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Text(nouns.last),
+          child: Column(
+            children: [
+              FlatButton(
+                onPressed: () {
+                  playSound("note1.wav");
+                },
+                color: Colors.red,
+                child: Text(""),
+              ),
+              FlatButton(
+                onPressed: () {
+                  playSound("note2.wav");
+                },
+                color: Colors.orange,
+                child: Text(""),
+              ),
+              FlatButton(
+                onPressed: () {
+                  playSound("note3.wav");
+                },
+                color: Colors.yellow,
+                child: Text(""),
+              ),
+              FlatButton(
+                onPressed: () {
+                  playSound("note4.wav");
+                },
+                color: Colors.blue,
+                child: Text(""),
+              ),
+              FlatButton(
+                onPressed: () {
+                  playSound("note5.wav");
+                },
+                color: Colors.green,
+                child: Text(""),
+              ),
+              FlatButton(
+                onPressed: () {
+                  playSound("note6.wav");
+                },
+                color: Colors.indigo,
+                child: Text(""),
+              ),
+              FlatButton(
+                onPressed: () {
+                  playSound("note7.wav");
+                },
+                color: Colors.purple,
+                child: Text(""),
+              ),
+            ],
+          ),
         ),
       ),
     );
