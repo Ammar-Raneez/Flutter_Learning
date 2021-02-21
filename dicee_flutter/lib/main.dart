f43d34615a45281f32ce7a9902aa2f10b73573e1
Ammar <ammarraneez@gmail.com>
2021-02-21 18:28:52 +0530

    Randomly generates dice from 1 to 6

--- dicee_flutter/lib/main.dart
+++ dicee_flutter/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -15,22 +16,69 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+//class DicePage extends StatelessWidget {
+//  int leftDice = 4;
+//  int rightDice = 3;
+//
+//  @override
+//  Widget build(BuildContext context) {
+//    return Center(
+//      child: Row(
+//        children: <Widget>[
+//          Expanded(
+//            child: FlatButton(
+//              onPressed: (){
+//                Image.asset("images/dice$leftDice.png");
+//              },
+//              child: Image.asset("images/dice1.png"),
+//            ),
+//          ),
+//          Expanded(
+//            child: FlatButton(
+//              onPressed: (){
+//                Image.asset("images/dice$rightDice.png");
+//              },
+//              child: Image.asset("images/dice1.png"),
+//            ),
+//          )
+//        ],
+//      ),
+//    );
+//  }
+//}
+
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDice = 1;
+  int rightDice = 1;
+
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Row(
         children: <Widget>[
           Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Image.asset("images/dice1.png"),
+            child: FlatButton(
+              onPressed: (){
+                setState(() {
+                  leftDice = Random().nextInt(6) + 1;
+                });
+              },
+              child: Image.asset("images/dice$leftDice.png"),
             ),
           ),
           Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Image.asset("images/dice1.png"),
+            child: FlatButton(
+              onPressed: (){
+                setState(() {
+                  rightDice = Random().nextInt(6) + 1;
+                });
+              },
+              child: Image.asset("images/dice$rightDice.png"),
             ),
           )
         ],
