39e98d106db5324214a5a5ddea693136b637b57d
Ammar <ammarraneez@gmail.com>
2021-02-21 23:21:47 +0530

    done but repeated code

--- xylophone_flutter/lib/main.dart
+++ xylophone_flutter/lib/main.dart
@@ -16,55 +16,70 @@ class XylophoneApp extends StatelessWidget {
       home: Scaffold(
         body: SafeArea(
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              FlatButton(
-                onPressed: () {
-                  playSound("note1.wav");
-                },
-                color: Colors.red,
-                child: Text(""),
+              Expanded(
+                child: FlatButton(
+                  onPressed: () {
+                    playSound("note1.wav");
+                  },
+                  color: Colors.red,
+                  child: Text(""),
+                ),
               ),
-              FlatButton(
-                onPressed: () {
-                  playSound("note2.wav");
-                },
-                color: Colors.orange,
-                child: Text(""),
+              Expanded(
+                child: FlatButton(
+                  onPressed: () {
+                    playSound("note2.wav");
+                  },
+                  color: Colors.orange,
+                  child: Text(""),
+                ),
               ),
-              FlatButton(
-                onPressed: () {
-                  playSound("note3.wav");
-                },
-                color: Colors.yellow,
-                child: Text(""),
+              Expanded(
+                child: FlatButton(
+                  onPressed: () {
+                    playSound("note3.wav");
+                  },
+                  color: Colors.yellow,
+                  child: Text(""),
+                ),
               ),
-              FlatButton(
-                onPressed: () {
-                  playSound("note4.wav");
-                },
-                color: Colors.blue,
-                child: Text(""),
+              Expanded(
+                child: FlatButton(
+                  onPressed: () {
+                    playSound("note4.wav");
+                  },
+                  color: Colors.blue,
+                  child: Text(""),
+                ),
               ),
-              FlatButton(
-                onPressed: () {
-                  playSound("note5.wav");
-                },
-                color: Colors.green,
-                child: Text(""),
+              Expanded(
+                child: FlatButton(
+                  onPressed: () {
+                    playSound("note5.wav");
+                  },
+                  color: Colors.green,
+                  child: Text(""),
+                ),
               ),
-              FlatButton(
-                onPressed: () {
-                  playSound("note6.wav");
-                },
-                color: Colors.indigo,
-                child: Text(""),
+              Expanded(
+                child: FlatButton(
+                  onPressed: () {
+                    playSound("note6.wav");
+                  },
+                  color: Colors.indigo,
+                  child: Text(""),
+                ),
               ),
-              FlatButton(
-                onPressed: () {
-                  playSound("note7.wav");
-                },
-                color: Colors.purple,
-                child: Text(""),
+              Expanded(
+                child: FlatButton(
+                  onPressed: () {
+                    playSound("note7.wav");
+                  },
+                  color: Colors.purple,
+                  child: Text(""),
+                ),
               ),
             ],
           ),
