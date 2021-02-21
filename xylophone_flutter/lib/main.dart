4d37f8c8037cf2bd7aaa11cbeddb0242528df261
Ammar <ammarraneez@gmail.com>
2021-02-21 21:39:46 +0530

    starter code for xylophone app

---
+++ xylophone_flutter/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+void main() => runApp(XylophoneApp());
+
+class XylophoneApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Text(nouns.last),
+        ),
+      ),
+    );
+  }
+}
