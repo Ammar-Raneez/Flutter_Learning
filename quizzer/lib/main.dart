72b3904f7c4760b5fba8a27667fa1d86cf35983a
Ammar <ammarraneez@gmail.com>
2021-02-28 15:23:00 +0530

    moving to displaying an alert message on quiz end

--- quizzer/lib/main.dart
+++ quizzer/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+//TODO: Step 2 - Import the rFlutter_Alert package here.
 import 'package:quizzler/QuestionBank.dart';
 
 void main() => runApp(Quizzler());
@@ -32,29 +33,29 @@ class _QuizPageState extends State<QuizPage> {
   void _checkAnswer(bool userPickedAnswer) {
     bool correctAnswer = questionBank.getQuestionAnswer();
 
-    if (correctAnswer == userPickedAnswer) {
-      setState(() {
-        this.scoreKeeper.add(
-              Icon(
-                Icons.check,
-                color: Colors.green,
-              ),
-            );
-      });
-    } else {
-      setState(() {
-        this.scoreKeeper.add(
-              Icon(
-                Icons.close,
-                color: Colors.red,
-              ),
-            );
-      });
-    }
     setState(() {
+      //TODO: Step 4 - Use IF/ELSE to check if we've reached the end of the quiz. If true, execute Part A, B, C, D.
+      //TODO: Step 4 Part A - show an alert using rFlutter_alert (remember to read the docs for the package!)
+      //HINT! Step 4 Part B is in the quiz_brain.dart
+      //TODO: Step 4 Part C - reset the questionNumber,
+      //TODO: Step 4 Part D - empty out the scoreKeeper.
+
+      //TODO: Step 5 - If we've not reached the end, ELSE do the answer checking steps below 👇
+      if (userPickedAnswer == correctAnswer) {
+        scoreKeeper.add(Icon(
+          Icons.check,
+          color: Colors.green,
+        ));
+      } else {
+        scoreKeeper.add(Icon(
+          Icons.close,
+          color: Colors.red,
+        ));
+      }
       questionBank.canNextQuestion();
     });
   }
+
 
   Expanded _userCard(String text, MaterialColor buttonColor, bool cardAnswer) {
     return Expanded(
